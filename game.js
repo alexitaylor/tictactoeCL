773604708adcc8a7bd84938b76fef2243c5b9c88
alexitaylor <alexi@bluesunrise.com>
2017-06-19 15:19:31 -0700

    Simple implementation of Tic Tac Toe game

--- game.js
+++ game.js
@@ -16,12 +16,59 @@ var board = {
   9: '  9 ',
 };
 
-function game(payerTurn) {
+function updateBoard(position, player) {
+  board[position] = '  ' + player + ' ';
+}
+
+function playerWon() {
+  if (board[1] === board[2] && board[1] === board[3]) {
+    return true;
+  } else if (board[4] === board[5] && board[4] === board[6]) {
+    return true;
+  } else if (board[7] === board[8] && board[7] === board[9]) {
+    return true;
+  } else if (board[1] === board[4] && board[1] === board[7]) {
+    return true;
+  } else if (board[2] === board[5] && board[2] === board[8]) {
+    return true;
+  } else if (board[3] === board[6] && board[3] === board[9]) {
+    return true;
+  } else if (board[1] === board[5] && board[1] === board[9]) {
+    return true;
+  } else if (board[3] === board[5] && board[3] === board[7]) {
+    return true
+  } else {
+    return false;
+  }
+}
+
+function game(player) {
   prompt.start();
 
   prompt.get(['position'], function(err, result) {
+    updateBoard(result.position, player);
     console.log('  user move: ' + result.position);
-    game();
+
+    // print new board
+    console.log(
+      board[1] + '|' + board[2] + '|' + board[3]
+      + '\n--------------'
+      + '\n' + board[4] + '|' + board[5] + '|' + board[6]
+      + '\n--------------'
+      + '\n' + board[7] + '|' + board[8] + '|' + board[9]
+    );
+
+    if (playerWon()) {
+      console.log('Player ' + player + ' Won!!!')
+    } else {
+      // next turn
+      if (player === 'X') {
+        game('O');
+      } else {
+        game('X');
+      }
+    }
+
   });
 }
 
